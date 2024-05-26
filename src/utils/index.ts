aadf8cc0a452541a102fd8861b96c1d810199df8
hasan814 <h.mousavi910@gmail.com>
2024-05-26 12:18:38 +0330

    Added GenerateCarImageUrl Function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
   const headers = {
     "x-rapidapi-key": "e69a76a7e1msh1b2249160a3b400p119898jsn2162ec9ed8f7",
@@ -22,4 +24,17 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
   return rentalRatePerDay.toFixed(2);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, year, model } = car;
+
+  url.searchParams.append("customer", "hrJavascript-lee");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
+  return `${url}`;
 };
